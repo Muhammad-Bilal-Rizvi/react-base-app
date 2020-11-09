32cd11aa4bd0bc8bf847332709a60f1c44e4c04d
Muhammad Bilal Rizvi <73776975+Muhammad-Bilal-Rizvi@users.noreply.github.com>
2020-11-08 17:50:42 -0800

    updated like first time
    updated like first time

--- src/App.js
+++ src/App.js
@@ -10,10 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
           <br/>
           updated by Bilal 
-          <br/>
-          again updated
-          <br/>
-          third time updated
+          
         </p>
         <a
           className="App-link"
